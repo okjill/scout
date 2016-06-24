320d732b276f0a5bd981a04ac150c287739dfc53
emritcey <emritcey@gmail.com>
2016-06-24 16:33:33 -0500

    add fake loader

--- background.js
+++ background.js
@@ -1,6 +1,15 @@
 $(document).ready(function(){
   $("body").css("background", "darkgray");
 
+  $("#fakeLoader").fakeLoader({
+            timeToHide:1200, //Time in milliseconds for fakeLoader disappear
+            zIndex:999, // Default zIndex
+            spinner:"spinner1",//Options: 'spinner1', 'spinner2', 'spinner3', 'spinner4', 'spinner5', 'spinner6', 'spinner7' 
+            bgColor:"#6E6464", //Hex, RGB or RGBA colors
+            // imagePath:"yourPath/customizedImage.gif" //If you want can you insert your custom image       
+    });
+
+
   var response = $.ajax({url: "https://api.flickr.com/services/rest/?method=flickr.favorites.getPublicList&api_key=e4aee99e08367dcf3791594e042828f2&user_id=87845824%40N05&extras=tags&format=json&nojsoncallback=1&auth_token=72157670098762976-5668f4a4d757dba0&api_sig=8d79789f68129a8d2aadf865f4fd355d", method: "get"});
 
   var allPhotos = response.done(function(photos) {
