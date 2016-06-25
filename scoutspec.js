ecebeffc12c0d19ec71d7442436dd4c8eecc24e4
emritcey <emritcey@gmail.com>
2016-06-25 09:35:41 -0500

    add firebase extra files

--- scoutspec.js
+++ scoutspec.js
@@ -5,7 +5,16 @@ describe("test to test the tests", function(){
 });
 
 describe("Scout Home Page", function(){
-	it("sh", function(){
+	it("shows an image", function(){
 		expect().to();
-	})
-})
+	});
+
+		it("shows a menu on the bottom", function(){
+		expect(document).to();
+	});
+
+
+	it("the menu is clickable", function(){
+		expect().to();
+	});
+});
