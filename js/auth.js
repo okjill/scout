472c47aeca555cdffbae67f81c25671d47794463
Jennifer Cross <nearlyjen@gmail.com>
2016-06-25 10:37:01 -0500

    auth with pop-up, got rid of blue focus box

---
+++ js/auth.js
@@ -0,0 +1,29 @@
+
+$(document).ready(function(){
+
+  var provider = new firebase.auth.GoogleAuthProvider();
+  // Google pop-up to sign in
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a Google Access Token. You can use it to access the Google API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+  // ...
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+
+  // Sign out
+  firebase.auth().signOut().then(function() {
+    // Sign-out successful.
+  }, function(error) {
+    // An error happened - should...do something.
+  });
+});
