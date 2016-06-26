b06e6786aa69dd266dadc37aad620f29cda908e1
okjill <okjill@gmail.com>
2016-06-26 01:35:22 -0500

    more work on functions

--- sandbox/jill_sat.js
+++ sandbox/jill_sat.js
@@ -1,24 +1,53 @@
 var ref = new Firebase('https://scout-1d20f.firebaseio.com');
 var usersRef = ref.child('users');
+// NEED TO REPLACE WITH CURRENT USER UID
+var uid = "jill";
+var currentUserNode = usersRef.child(uid + '/destinations');
 var destinationsRef = ref.child('destinations');
 
-function showUserDestinations(uid) {
-  usersRef.child(uid + '/destinations').orderByKey().on('child_added', function(snapshot){
-      destinationsRef.orderByKey().equalTo(snapshot.key()).on('child_added', function(snapshot){
-          var destinationName = snapshot.val().name;
-          $('#user-destinations').append('<p>' + destinationName + '</p>');
-      });
+function showUserDestinations() {
+  currentUserNode.orderByKey().on('child_added', function(snapshot){
+    destinationsRef.orderByKey().equalTo(snapshot.key()).on('child_added', function(snapshot){
+        var destinationName = snapshot.val().name;
+        $('#user-destinations').append('<p>' + destinationName + '</p>');
+    });
   });
 }
 
 function showAllDestinations() {
   destinationsRef.on("child_added", function(snapshot){
-    $("#all-destinations").append("<p>" + snapshot.val().name + "<span><button onclick=\"saveDestination('" + snapshot.key() + "')\">Save</button></span></p>");
+    var destinationName = snapshot.val().name;  
+    var destinationKey = snapshot.key();
+
+    $("#all-destinations").append("<p>" + destinationName + " <button onclick=\"saveDestination('" + destinationKey + "')\">Save</button></p>");
   });
 }
 
 function saveDestination(destination) {
-  uid = "jill";
-  usersRef.child(uid + '/destinations').update({[destination]: true});
+  currentUserNode.update({[destination]: true});
   destinationsRef.child(destination).child('users').update({[uid]: true});
 }
+
+function deleteDestination(place) {
+  function placeToDelete() {
+    return place;
+  };
+
+  // delete user's destination
+    currentUserNode.orderByKey().on('child_added', function(snapshot) {
+    
+    snapshot.forEach(function(destination){
+      if(destination.key() == placeToDelete()) {
+        var snapshotKey = snapshot.key();
+        var removeRef = usersRef.child("sangmee/destinations/" + snapshotKey + "/" + placeToDelete());
+        removeRef.remove();
+      };
+    });
+  });
+
+  // delete destination's user
+    destinationsRef.orderByKey().equalTo(placeToDelete()).on('child_added', function(snapshot) {
+    var removeRef = destinationsRef.child(placeToDelete() + "/users/" + uid);
+    removeRef.remove();
+  });
+};
